cb57a7cde8bb8b8d0eeca36746c7a921c36241ef
Rishav Jindal <rishavj0212@gmail.com>
2023-12-29 03:47:14 +0530

    .

--- first_project/src/app/page.jsx
+++ first_project/src/app/page.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import styles from './page.module.css'
 import { useRouter } from 'next/navigation'
+import { Button } from '@mui/material'
 
 export default function Home() {
   const router = useRouter();
@@ -11,6 +12,7 @@ export default function Home() {
       <Link href="/login">Go to Login page</Link>
       <Link href="/aboutus">Go to About Us page</Link>
       <button onClick={()=>router.push("/login")}>Login Page</button>
+      <Button variant='Contained'> Hello World</Button>
     </div>
   )
 } 
